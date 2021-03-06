53ab6d0e6a8897f7964162d84925ac462a4561c1
karloprodan <karloprodan@gmail.com>
2021-03-06 16:02:39 +0100

    otklonjen problem ne prikazivanja description-a

--- src/views/EventInfo.vue
+++ src/views/EventInfo.vue
@@ -147,7 +147,6 @@ export default {
         numLiked: 0,
         watched: false,
         numWatched: 0,
-        descriptionOpen: false,
       };
     } else {
       // inace baca err u konzolu: can't read email of undefined
@@ -159,6 +158,7 @@ export default {
         numLiked: 0,
         watched: false,
         numWatched: 0,
+        descriptionOpen: false,
       };
     }
   },
